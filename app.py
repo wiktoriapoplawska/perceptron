a7ed26d9ca7b87fe67385f601e97d3bfd7fdf543
wiktoriapoplawska <wikapoplawska@gmail.com>
2024-05-25 18:12:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,25 +51,6 @@ def get_prediction():
     # Return a json object containing the features and prediction
     return jsonify(features=features, predicted_class=predicted_class)
 
-@app.route('/predict_post', methods=['POST'])
-def post_predict():
-    data = request.get_json(force=True)
-    # sepal length
-    sepal_length = float(data.get('sl'))
-    petal_length = float(data.get('pl'))
-    
-    features = [sepal_length, petal_length]
-
-    # Load pickled model file
-    with open('model.pkl',"rb") as picklefile:
-        model = pickle.load(picklefile)
-        
-    # Predict the class using the model
-    predicted_class = int(model.predict(features))
-    output = dict(features=features, predicted_class=predicted_class)
-    # Return a json object containing the features and prediction
-    return jsonify(output)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
